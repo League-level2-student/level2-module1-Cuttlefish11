eee30102d7418da4b8262fa1db6bb131cb65bd5e
Cuttlefish11 <68820789+Cuttlefish11@users.noreply.github.com>
2022-01-17 18:23:39 -0800

    can you see this?

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,12 +1,23 @@
 package _00_intro_to_array_lists;
 
+import javax.swing.JButton;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+String[] texts = new String[5];
 		//2. Add five Strings to your list
-		
+for (int i = 0; i < texts.length; i++) {
+	texts[i] = new String ();
+	texts[i]= "ehdfdhrseshesh";
+	System.out.println(texts[i]);
+}
+for (String a: texts) {
+	int b = Integer.parseInt(a);
+	 System.out.println(texts[b]);
+}
+//ASK HOW IN THE WORLD FOR EACH LOOPS EVEN WORK!?! I mean, who knows?
 		//3. Print all the Strings using a standard for-loop
 		
 		//4. Print all the Strings using a for-each loop
